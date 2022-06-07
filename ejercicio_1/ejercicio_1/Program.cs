6febfdb86833d4039b10b6805fcd8e826ef3b4bd
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-06-07 15:12:15 -0300

    primero

--- ejercicio_1/ejercicio_1/Program.cs
+++ ejercicio_1/ejercicio_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        string? path;
+        Console.WriteLine("Ingrese una ruta:");
+        path = Console.ReadLine();
+        mostrarDirectorio(path);
+
+    }
+
+    public static void mostrarDirectorio(string path)
+    {
+        if (Directory.Exists(path)){
+            List<string> listadoCarpetas = Directory.GetDirectories(path).ToList();
+            foreach(string CarpetaY in listadoCarpetas)
+            {
+                Console.WriteLine(CarpetaY);
+
+                foreach(string item in Directory.GetFiles(CarpetaY))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+
+    }
+}
