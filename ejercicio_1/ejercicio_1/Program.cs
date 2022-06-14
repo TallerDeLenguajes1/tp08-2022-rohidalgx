e3713ec4b8fbb83872097f8f1449f875b2c20a16
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-06-14 16:05:29 -0300

    listo

--- ejercicio_1/ejercicio_1/Program.cs
+++ ejercicio_1/ejercicio_1/Program.cs
@@ -1,34 +1,37 @@
-﻿using System;
-using System.IO;
+﻿Console.WriteLine("Ingrese un path");
 
+string? carpetaPath = Console.ReadLine();
 
+if (!Directory.Exists(carpetaPath)) return;
 
-public class program
+List<string> listadoDeCarpetas = Directory.GetDirectories(carpetaPath).ToList();
+string nombreArchivo = carpetaPath + @"\index.csv";
+FileStream fileStream;
+
+if (!File.Exists(nombreArchivo))
 {
-    public static void Main(string[] args)
-    {
-        string? path;
-        Console.WriteLine("Ingrese una ruta:");
-        path = Console.ReadLine();
-        mostrarDirectorio(path);
+    fileStream = File.Create(nombreArchivo);
+    fileStream.Close();
+}
 
+using (fileStream = new FileStream(nombreArchivo, FileMode.OpenOrCreate))
+using (var streamWriter = new StreamWriter(fileStream))
+{
+    var contador = 0;
+
+    foreach (string carpeta in listadoDeCarpetas)
+    {
+        streamWriter.Write(contador + ";");
+        streamWriter.WriteLine(new DirectoryInfo(carpeta).Name + ";");
+        contador++;
     }
 
-    public static void mostrarDirectorio(string path)
+    foreach (string archivo in Directory.GetFiles(carpetaPath))
     {
-        if (Directory.Exists(path)){
-            List<string> listadoCarpetas = Directory.GetDirectories(path).ToList();
-            foreach(string CarpetaY in listadoCarpetas)
-            {
-                Console.WriteLine(CarpetaY);
-
-                foreach(string item in Directory.GetFiles(CarpetaY))
-                {
-                    Console.WriteLine(item);
-                }
-            }
-
-        }
-
+        Console.WriteLine(archivo);
+        streamWriter.Write(contador + ";");
+        streamWriter.Write(Path.GetFileNameWithoutExtension(archivo) + ";" + Path.GetExtension(archivo) + "\n");
+        contador++;
     }
+
 }
